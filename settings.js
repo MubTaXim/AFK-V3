e9b4f10b2c400d4886457dcff37bdf8c938072b3
Mubtasim Hasan <70112659+MubTaXim@users.noreply.github.com>
2024-10-11 08:50:52 +0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 // settings.js
 
 module.exports = {
-  HOST: 'move_gay_69.aternos.me',  // Server IP
-  PORT: 32150,                     // Server Port
+  HOST: 'yourserver.aternos.me',  // Server IP
+  PORT: yourport,                     // Server Port
   HTTP_PORT: process.env.PORT || 3000, // HTTP server port
   RECONNECT_DELAY: 6000,           // Reconnect delay (in milliseconds)
   SECOND_BOT_DELAY: 20000,         // Delay before creating the second bot (in milliseconds)
