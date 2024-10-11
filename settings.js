cc8841d1c1d8a6932d204479bd87c7984125438f
Mubtasim Hasan <70112659+MubTaXim@users.noreply.github.com>
2024-10-11 08:16:16 +0600

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,14 @@
+// settings.js
+
+module.exports = {
+  HOST: 'move_gay_69.aternos.me',  // Server IP
+  PORT: 32150,                     // Server Port
+  HTTP_PORT: process.env.PORT || 3000, // HTTP server port
+  RECONNECT_DELAY: 6000,           // Reconnect delay (in milliseconds)
+  SECOND_BOT_DELAY: 20000,         // Delay before creating the second bot (in milliseconds)
+  MINECRAFT_VERSION: '1.20.1',     // Minecraft version
+  BOT_NAMES: {
+    FIRST_BOT: 'BotOne',           // First bot name
+    SECOND_BOT: 'BotTwo',           // Second bot name
+  }
+};
